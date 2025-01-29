85bfba1c58f8425f3d1acb084ed02d2a95e35c5b
BRAYWAYDOOR <147653436+BRAYWAYDOOR@users.noreply.github.com>
2025-01-29 16:51:33 +0000

    First commit

---
+++ src/MixMatch.java
@@ -0,0 +1,22 @@
+public class MixMatch{
+    public static void main(String args[]){
+        //variable declaration
+        String word = "This is a string";
+        char letter = 'b';
+        short num = 9;
+        int num2 = 9;
+        long num3 = 9;
+        float decimal = 9.0f;
+        double decimal2 = 9.0;
+        boolean willPrint = true;
+        //print statements
+        System.out.println(word + " <- This is a string variable type");
+        System.out.println(letter + " <- This is a character variable type");
+        System.out.println(num + " <- This is a short variable type");
+        System.out.println(num2 + " <- This is an integer variable type");
+        System.out.println(num3 + " <- This is a long variable type");
+        System.out.println(decimal + " <- This is a float variable type");
+        System.out.println(decimal2 + " <- This is a double variable type");
+        System.out.println(willPrint + " <- This is a boolean variable type");
+    }
+}
